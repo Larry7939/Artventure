f37641d9b1620d7bd7cb4519f5655794a7489f15
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-05 17:58:01 +0900

    [chore] ApplicationClass 추가

---
+++ app/src/main/java/com/artventure/artventure/application/ApplicationClass.kt
@@ -0,0 +1,14 @@
+package com.artventure.artventure.application
+
+import android.app.Application
+import com.artventure.artventure.BuildConfig
+import timber.log.Timber
+
+
+class ApplicationClass : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
