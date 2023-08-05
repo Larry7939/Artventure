bfd87ca19938d0c06e10c24ae0e5f29a1d97f763
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-05 23:25:26 +0900

    [feat] Splash Screen 구현
     - nav_version 버전 변경 2.5.3 -> 2.6.0
     - Splashscreen api 의존성 추가
     - 런쳐 아이콘 추가
     - Splash Theme 추가

--- app/src/main/java/com/artventure/artventure/MainActivity.kt
+++ app/src/main/java/com/artventure/artventure/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.artventure.artventure
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.artventure.artventure.binding.BindingActivity
 import com.artventure.artventure.databinding.ActivityMainBinding
-import timber.log.Timber
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
-
+    private lateinit var splashScreen: SplashScreen
     override fun onCreate(savedInstanceState: Bundle?) {
+        splashScreen = installSplashScreen()
+        startSplash()
         super.onCreate(savedInstanceState)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -17,6 +25,20 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         addNavigationListener(navController)
     }
 
+    private fun startSplash() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f, 2.5f,1f)
+            val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f, 2.5f,1f)
+            ObjectAnimator.ofPropertyValuesHolder(splashScreenView.iconView, scaleX, scaleY).run {
+                interpolator = AnticipateInterpolator()
+                duration = SPLASH_DURATION
+                doOnEnd {
+                    splashScreenView.remove()
+                }
+                start()
+            }
+        }
+    }
 
     private fun addNavigationListener(navController: NavController) {
         var selectedItemId = R.id.home
@@ -39,5 +61,8 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
                 }
             }
         }
+    }
+    companion object{
+        const val SPLASH_DURATION =1500L
     }
 }
