7f16f9503efe77d5381ab334985f988b5df744f2
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-07 17:05:33 +0900

    [feat] 검색 결과 무한 스크롤 구현
     - 검색 수행 시 검색 인덱스 초기화
     - 결과 맨 끝에서 스크롤 시 추가 데이터 로딩
     - 페이징 프로그래스 노출 구현
     - 어댑터 초기화 후 스크롤 복원 구현

--- app/src/main/java/com/artventure/artventure/presentation/SearchViewModel.kt
+++ app/src/main/java/com/artventure/artventure/presentation/SearchViewModel.kt
@@ -18,16 +18,19 @@ import javax.inject.Inject
 class SearchViewModel @Inject constructor(private val searchRepositoryImpl: SearchRepository) :
     ViewModel() {
     private var _isSearchEnable = MutableLiveData(false)
-    val isSearchEnable:LiveData<Boolean>
+    val isSearchEnable: LiveData<Boolean>
         get() = _isSearchEnable
-    val onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
+    val onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
         _isSearchEnable.value = hasFocus
     }
-    private var _searchWordEmptyResult: MutableLiveData<String> = MutableLiveData<String>()
-    val searchWordEmptyResult:LiveData<String>
-        get() = _searchWordEmptyResult
+    private var _searchWord: MutableLiveData<String> = MutableLiveData<String>()
+    val searchWord: LiveData<String>
+        get() = _searchWord
 
-    val searchWord: MutableLiveData<String> = MutableLiveData<String>()
+    val searchWordInput: MutableLiveData<String> = MutableLiveData<String>()
+
+    private var startIdx = START_INDEX
+    private var endIdx = END_INDEX
 
     private var _collections = ListLiveData<CollectionDto>()
     val collections: ListLiveData<CollectionDto>
@@ -37,36 +40,86 @@ class SearchViewModel @Inject constructor(private val searchRepositoryImpl: Sear
     val searchState: LiveData<UiState>
         get() = _searchState
 
+    private var _pagingState = MutableLiveData<UiState>()
+    val pagingState: LiveData<UiState>
+        get() = _pagingState
+
     private var _totalCount = 0
     val totalCount: Int
         get() = _totalCount
 
-    var code = ""
+    private var code = ""
 
     fun searchCollection() {
         viewModelScope.launch {
+            _searchWord.value = searchWordInput.value
             _searchState.value = UiState.LOADING
             runCatching {
                 searchRepositoryImpl.getCollections(
-                    startIdx = 1,
-                    endIdx = 100,
-                    searchWord = searchWord.value.toString()
+                    startIdx = START_INDEX,
+                    endIdx = END_INDEX,
+                    searchWord = searchWordInput.value.toString()
                 )
             }.onSuccess { result ->
                 if (result.searchCollectionInfo != null) {
-                    _collections.value = result.searchCollectionInfo.infoList.map { it.toCollection() } as MutableList<CollectionDto>
+                    _collections.value =
+                        result.searchCollectionInfo.infoList.map { it.toCollection() } as MutableList<CollectionDto>
                     _totalCount = result.searchCollectionInfo.totalCount
                     code = result.searchCollectionInfo.result.code
                     _searchState.value = UiState.SUCCESS
                 } else {
-                    Timber.d("Empty")
-                    _searchWordEmptyResult.value = searchWord.value
-                    _searchState.value = UiState.EMPTY
+                    if (result.result?.code == "INFO-200") {
+                        _searchState.value = UiState.EMPTY
+                    }
                 }
             }.onFailure {
                 Timber.e("Search Failed ${it.message}")
                 _searchState.value = UiState.ERROR
             }
         }
+    }
+
+    fun pagingCollection() {
+        viewModelScope.launch {
+            _pagingState.value = UiState.LOADING
+            runCatching {
+                searchRepositoryImpl.getCollections(
+                    startIdx = startIdx,
+                    endIdx = endIdx,
+                    searchWord = _searchWord.value.toString()
+                )
+            }.onSuccess { result ->
+                if (result.searchCollectionInfo != null) {
+                    _collections.value?.addAll(result.searchCollectionInfo.infoList.map { it.toCollection() } as MutableList<CollectionDto>)
+                    Timber.d("Paging Success! size => ${_collections.value?.size}")
+                    _pagingState.value = UiState.SUCCESS
+                } else {
+                    if (result.result?.code == "INFO-200") {
+                        _pagingState.value = UiState.EMPTY
+                    }
+                }
+            }.onFailure {
+                Timber.e("Paging Failed ${it.message}")
+                _pagingState.value = UiState.ERROR
+            }
+        }
+    }
+
+    fun initSearchIndex() {
+        startIdx = START_INDEX
+        endIdx = END_INDEX
+    }
+
+    fun updateSearchIndex() {
+        startIdx = endIdx + 1
+        endIdx += PAGE_SIZE
+    }
+
+    fun checkIndexValidation(): Boolean = startIdx <= totalCount
+
+    companion object {
+        const val START_INDEX = 1
+        const val END_INDEX = 100
+        const val PAGE_SIZE = 100
     }
 }
--- app/src/main/java/com/artventure/artventure/presentation/screen/SearchFragment.kt
+++ app/src/main/java/com/artventure/artventure/presentation/screen/SearchFragment.kt
@@ -1,6 +1,8 @@
 package com.artventure.artventure.presentation.screen
 
 import android.os.Bundle
+import android.os.Parcelable
+import android.os.SystemClock
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
@@ -9,6 +11,8 @@ import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.artventure.artventure.R
 import com.artventure.artventure.binding.BindingFragment
 import com.artventure.artventure.databinding.FragmentSearchBinding
@@ -28,6 +32,8 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_
     private val adapter by lazy {
         SearchAdapter(requireContext())
     }
+    private lateinit var recyclerViewState: Parcelable
+
     private val backPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
             moveToHome()
@@ -59,6 +65,7 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_
                     if (view?.text?.isNotEmpty() == true) {
                         requireContext().clearFocus(binding.etSearch)
                         searchViewModel.searchCollection()
+                        searchViewModel.initSearchIndex()
                     } else {
                         warningEmptySearchWord()
                     }
@@ -68,14 +75,63 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_
             }
         })
         binding.btnSearch.setOnClickListener {
-            if(binding.etSearch.text?.isNotEmpty() == true){
+            if (binding.etSearch.text?.isNotEmpty() == true) {
                 requireContext().clearFocus(binding.etSearch)
                 searchViewModel.searchCollection()
-            }
-            else{
+                searchViewModel.initSearchIndex()
+            } else {
                 warningEmptySearchWord()
             }
 
+        }
+        setSearchPagingListener()
+    }
+
+    private fun setSearchPagingListener() {
+        /**스크롤의 위치가 마지막 아이템의 위치와 동일할 시 추가 index 호출*/
+        binding.rvSearchResult.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            private var lastScrollTime = 0
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                    if (!recyclerView.canScrollVertically(1)) {
+                        val lastPosition =
+                            (recyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+                        val totalCount = recyclerView.adapter?.itemCount
+                        /**스크롤 이벤트 중복 호출 방지*/
+                        if (SystemClock.elapsedRealtime() - lastScrollTime > SCROLL_TIME) {
+                            if (totalCount != null) {
+                                if (lastPosition == totalCount - 1 && searchViewModel.checkIndexValidation()) {
+                                    /**인덱스 증가 및 페이징 호출*/
+                                    searchViewModel.updateSearchIndex()
+                                    searchViewModel.pagingCollection()
+                                }
+                            }
+                        }
+                        lastScrollTime = SystemClock.elapsedRealtime().toInt()
+                    }
+                }
+
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                /**페이징 후 스크롤 초기화 시에 사용될 스크롤 위치 저장*/
+                recyclerViewState =
+                    (recyclerView.layoutManager as LinearLayoutManager).onSaveInstanceState()!!
+            }
+        })
+    }
+
+    private fun pagingSearchResult() {
+        with(binding) {
+            initAdapter()
+            /**어댑터 초기화 후 스크롤 위치 복원*/
+            if (::recyclerViewState.isInitialized) {
+                (rvSearchResult.layoutManager as LinearLayoutManager).onRestoreInstanceState(
+                    recyclerViewState
+                )
+            }
         }
     }
 
@@ -87,6 +143,13 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_
         searchViewModel.searchState.observe(viewLifecycleOwner) { state ->
             if (state == UiState.SUCCESS) {
                 initAdapter()
+            }
+        }
+        searchViewModel.pagingState.observe(viewLifecycleOwner) { state ->
+            if (state == UiState.SUCCESS) {
+                pagingSearchResult()
+            } else if (state == UiState.EMPTY) {
+                requireContext().showToast(getString(R.string.warning_last_search_word))
             }
         }
     }
@@ -108,5 +171,8 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_
 
     private fun showMainBottomNav() {
         mainViewModel.setBottomNavVisibility(View.VISIBLE)
+    }
+    companion object{
+        const val SCROLL_TIME = 1000L
     }
 }
