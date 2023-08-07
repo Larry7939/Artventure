c8dba7619df53fa07ac6c443c02aa120ed5a3155
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-07 19:08:30 +0900

    [add] DetailActivity 추가

---
+++ app/src/main/java/com/artventure/artventure/presentation/screen/DetailActivity.kt
@@ -0,0 +1,53 @@
+package com.artventure.artventure.presentation.screen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.artventure.artventure.data.model.dto.CollectionDto
+import com.artventure.artventure.presentation.screen.ui.theme.ArtventureTheme
+import com.artventure.artventure.util.extension.customGetSerializable
+import timber.log.Timber
+
+class DetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val bundle = intent.getBundleExtra("search")
+        val data = bundle?.customGetSerializable<CollectionDto>("content")
+        Timber.d("$data")
+
+        setContent {
+            ArtventureTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ArtventureTheme {
+        Greeting("Android")
+    }
+}
