aec6c54920dfb0afdfa58189df6a924e87370fdc
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-08-07 21:09:10 +0900

    [add] Typography 추가

--- app/src/main/java/com/artventure/artventure/presentation/screen/ui/theme/Type.kt
+++ app/src/main/java/com/artventure/artventure/presentation/screen/ui/theme/Type.kt
@@ -2,33 +2,52 @@ package com.artventure.artventure.presentation.screen.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.artventure.artventure.R
 
+val pretendard = FontFamily(
+    Font(R.font.pretendard_b, FontWeight.Bold),
+    Font(R.font.pretendard_sb, FontWeight.SemiBold),
+    Font(R.font.pretendard_m, FontWeight.Medium),
+)
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+val ArtventureTypography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 15.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = pretendard,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 18.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp
     )
-    */
 )
